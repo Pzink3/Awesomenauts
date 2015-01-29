4ff65ccd19e61990d9dcd71d9046a0741c6a150d
Parker Zink <parker.zink@gmail.com>
2015-01-29 09:07:35 -0800

    Capitalized title
    Capitalized my title of my game

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 	<head>
-		<title>Awesomenauts</title>
+		<title>AWESOMENAUTS</title>
 		<link rel="stylesheet" type="text/css" media="screen" href="index.css"> <!-- calls the css index file -->
 		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 		<meta name="apple-mobile-web-app-capable" content="yes">
