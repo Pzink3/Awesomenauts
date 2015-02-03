73194fc4322f741984f8640f0328b83abf4c3051
Parker Zink <parker.zink@gmail.com>
2015-02-02 18:20:53 -0800

    Finally fixed title screen
    I finally fixed my title screen for getting it to work functionally!

--- js/resources.js
+++ js/resources.js
@@ -28,7 +28,7 @@ game.resources = [
 	 * @example
 	 * {name: "example_bgm", type: "audio", src: "data/bgm/"},
 	 */	
-
+           
           
 	/* Sound effects. 
 	 * @example
--- js/screens/play.js
+++ js/screens/play.js
@@ -6,7 +6,7 @@ game.PlayScreen = me.ScreenObject.extend({
 		// reset the score
 		game.data.score = 0;
 
-
+                
                 me.levelDirector.loadLevel("level01"); // calls the level director to load level
                 
        
