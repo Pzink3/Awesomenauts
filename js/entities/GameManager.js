fa2ef5d890ceb2fd39029da277da337a643deead
Parker Zink <parker.zink@gmail.com>
2015-03-05 08:49:05 -0800

    Finished buy menu
    Finished the buy menu from video 49

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -128,7 +128,13 @@ game.SpendGold = Object.extend({
          },
          
          draw: function(renderer){
-             this.font.draw(renderer.getContext(), "F1 - F6 to buy, B to exit");
+             this.font.draw(renderer.getContext(), "F1 - F6 to buy, B to exit, Current Gold: " + game.data.gold, this.pos.x, this.pos.y);
+              this.font.draw(renderer.getContext(), "Skill 1: Increase Damage, Current Level: " + game.data.skill1 + "Cost: " + ((game.data.exp1+1)*10), this.pos.x, this.pos.y + 40);
+               this.font.draw(renderer.getContext(), "Skill 2: Run Faster! Current Level: " + game.data.skill2 + "Cost: " + ((game.data.skill2+1)*10), this.pos.x, this.pos.y + 80);
+                this.font.draw(renderer.getContext(), "Skill 3: Increase Health. Current Level: " + game.data.skill3 + "Cost: " + ((game.data.skill3+1)*10), this.pos.x, this.pos.y + 120);
+                 this.font.draw(renderer.getContext(), "Q Ability: Speed Burst. Current Level: ", game.data.ability1 + "Cost: " + ((game.data.ability1+1)*10), this.pos.x, this.pos.y + 160);
+                  this.font.draw(renderer.getContext(), "W Ability: Eat Your Creep For Health: " + game.data.ability2 + "Cost: " + ((game.data.ability2+1)*10), this.pos.x, this.pos.y + 200);
+                   this.font.draw(renderer.getContext(), "E Ability: Throw Your Spear: " + game.data.ability3 + "Cost: " + ((game.data.ability3+1)*10), this.pos.x, this.pos.y + 240);
          }
      })));
      me.game.world.addChild(game.data.buytext, 35);
